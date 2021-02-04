6ce883b56f8eae01be9253c80dab8a7a0b76f170
reckypoo <67753346+reckypoo@users.noreply.github.com>
2021-02-03 23:11:01 -0600

    Initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+// predictable expenses into an associative array (auto_id, name, expense value, date value)
+
+// predictable income into an associative array (auto_id, name, income value, date value)
+
+// entries associative array (auto_id, name, balance, date value)
+
+//should display assumed current balance with entries data & other data in account. This would have to be AJAXed so new data can be entered, and effect the display.
