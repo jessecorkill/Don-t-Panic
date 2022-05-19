5d60402293b16299c9e9c7c2ea02a9184ac5aeb9
Jesse Corkill <xxjescorkillxx@gmail.com>
2022-05-19 15:52:31 -0500

    GraphQL initiated

--- dont-panic-budgeting/src/App.js
+++ dont-panic-budgeting/src/App.js
@@ -3,10 +3,33 @@ import logo from './logo.svg';
 import './App.css';
 
 //Function Component for Top Navigation
+function NavButton(props){
+  const self = this;
+  return(
+    <div id="nav_menu">
+      <button onClick={props.onClick}>Navigation Toggle</button>
+      <div className={props.navState}>
+        <div className="profile">
+          <h5>Your Name Here</h5>
+          <p>Not you?</p><button >Sign out!</button>
+        </div>
+        <ul>
+          <li><button >Edit Budget</button></li>
+          <li><button onClick={props.budgetNav}>Budget Outlook</button></li>
+        </ul>
+      </div>
+
+    </div>
+  )
+}
 
 //Function Component for Login View
 
 //Function Component for Creating Budget View
+function BudgetCalendarView(props){
+  
+}
+
 
 //Function Component for Calendar View
 
@@ -14,28 +37,43 @@ class App extends React.Component {
   constructor(props){
     super(props);
     this.state = {
+      loggedIn: true,
+      modalScreen: 'edit',
+      navState: 'hidden',
 
     }
+
+  }
+  componentDidMount(){
+      //if data is available, fetch data 
+      
+  }
+  //function for handling nav click
+  handleNavClick(self){
+    //check state of nav menu
+    if(self.state.navState === "hidden"){
+      self.setState({
+        navState: "show",
+      })
+    }else{
+      self.setState({
+        navState: "hidden",
+      })
+    }
+  }
+  //
+  handleBudgetOverlook(self){
+    self.setState({
+      modalScreen: "budget",
+    })
   }
 
   render(){
-    return (
+    return(
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <NavButton navState={this.state.navState} budgetNav={()=> this.handleBudgetOverlook(this)} onClick={() => this.handleNavClick(this)}></NavButton>
       </div>
+      
     );
   }
 
--- dont-panic-budgeting/src/budgetHandler.js
+++ dont-panic-budgeting/src/budgetHandler.js
@@ -4,116 +4,126 @@ const budget = require("../../src/myBudget.json");
 
 var expenses = budget.expenses;
 var income = budget.income;
+class Budget{
+  constructor(){
 
-//Set current Balance
-var todaysBal = 400;
-//Balance Prediction Var
-
-//Array for storing predicted balances for each day in 30 iterations
-let predictedBal = new Array(31).fill(30);
-//Array for storing associated Date() for each day in 30 iterations
-let assocDates = new Array(31).fill(null);
-//Array for storing details of what transpired on the predicted day
-let assocDetails = Array.from({ length: 31 }, () => []);
-
-//Insert todaysBal into first day of our new array
-predictedBal[0] = todaysBal;
-
-//Get current Date
-const today = new Date();
-
-//Loop through next 30 Days of expenses & income
-for (var index = 0; index <= 30; index++) {
-  //Get the day
-  var date = new Date(today);
-  date.setDate(date.getDate() + index);
-  //Record the Date obj in the assocDates array
-  assocDates[index] = date;
-
-  //Prime this day's balance with yesterday's balance, unless it is the first day.
-  if (index !== 0) {
-    //Previous day's balance
-    var prevDay = predictedBal[index - 1];
-    predictedBal[index] = prevDay;
   }
-  //Check budget for income
-  income.forEach((element) => {
-    if (element[2] === date.getDate() && element[0] !== "w") {
-      //Monthly Income
-      if (element[0] === "m") {
-        //Made money this day, so we add to this day's balance
-        predictedBal[index] = predictedBal[index] + element[3];
-        // Log element's description value to the description Array (compensate for muliple entries on a day)
-        assocDetails[index].push(element[1]);
-      } else if (element[0] === "b") {
-        //Bi-Weekly or Salary Income
-        predictedBal[index] = predictedBal[index] + element[3];
-        assocDetails[index].push(element[1]);
-        //Add same balance to the predictedBal 15 indexes over
-        //Has the date currently passed? If so target fifteen days behind the current index
-        if (today.getDate() > date.getDate()) {
-          predictedBal[index - 16] = predictedBal[index - 16] + element[3];
-          assocDetails[index - 16].push(element[1]);
+  budgetThirtyDays(todaysBal){
+    //Set current Balance
+  var todaysBal = 400;
+  //Balance Prediction Var
+  
+  //Array for storing predicted balances for each day in 30 iterations
+  let predictedBal = new Array(31).fill(30);
+  //Array for storing associated Date() for each day in 30 iterations
+  let assocDates = new Array(31).fill(null);
+  //Array for storing details of what transpired on the predicted day
+  let assocDetails = Array.from({ length: 31 }, () => []);
+  
+  //Insert todaysBal into first day of our new array
+  predictedBal[0] = todaysBal;
+  
+  //Get current Date
+  const today = new Date();
+  
+  //Loop through next 30 Days of expenses & income
+  for (var index = 0; index <= 30; index++) {
+    //Get the day
+    var date = new Date(today);
+    date.setDate(date.getDate() + index);
+    //Record the Date obj in the assocDates array
+    assocDates[index] = date;
+  
+    //Prime this day's balance with yesterday's balance, unless it is the first day.
+    if (index !== 0) {
+      //Previous day's balance
+      var prevDay = predictedBal[index - 1];
+      predictedBal[index] = prevDay;
+    }
+    //Check budget for income
+    income.forEach((element) => {
+      if (element[2] === date.getDate() && element[0] !== "w") {
+        //Monthly Income
+        if (element[0] === "m") {
+          //Made money this day, so we add to this day's balance
+          predictedBal[index] = predictedBal[index] + element[3];
+          // Log element's description value to the description Array (compensate for muliple entries on a day)
+          assocDetails[index].push(element[1]);
+        } else if (element[0] === "b") {
+          //Bi-Weekly or Salary Income
+          predictedBal[index] = predictedBal[index] + element[3];
+          assocDetails[index].push(element[1]);
+          //Add same balance to the predictedBal 15 indexes over
+          //Has the date currently passed? If so target fifteen days behind the current index
+          if (today.getDate() > date.getDate()) {
+            predictedBal[index - 16] = predictedBal[index - 16] + element[3];
+            assocDetails[index - 16].push(element[1]);
+          } else {
+            predictedBal[index + 15] = predictedBal[index + 15] + element[3];
+            assocDetails[index + 15].push(element[1]);
+          }
         } else {
-          predictedBal[index + 15] = predictedBal[index + 15] + element[3];
-          assocDetails[index + 15].push(element[1]);
+        }
+      } else if (element[0] === "w") {
+        //Get the targeted day of the week
+        let targetDay = element[2];
+        //Check if this day's weekday matches the targetDay
+        if (date.getDay() === targetDay) {
+          predictedBal[index] = predictedBal[index] + element[3];
+          assocDetails[index].push(element[1]);
         }
       } else {
+        //Element did not match index
       }
-    } else if (element[0] === "w") {
-      //Get the targeted day of the week
-      let targetDay = element[2];
-      //Check if this day's weekday matches the targetDay
-      if (date.getDay() === targetDay) {
-        predictedBal[index] = predictedBal[index] + element[3];
-        assocDetails[index].push(element[1]);
-      }
-    } else {
-      //Element did not match index
-    }
-  });
-  //Check budget for expenses
-  expenses.forEach((element) => {
-    if (element[2] === date.getDate() && element[0] !== "w") {
-      //Monthly Income
-      if (element[0] === "m") {
-        //Made money this day, so we add to this day's balance
-        predictedBal[index] = predictedBal[index] - element[3];
-        // Log element's description value to the description Array (compensate for muliple entries on a day)
-        assocDetails[index].push(element[1]);
-      } else if (element[0] === "b") {
-        //Bi-Weekly or Salary Income
-        predictedBal[index] = predictedBal[index] - element[3];
-        assocDetails[index].push(element[1]);
-        //Add same balance to the predictedBal 15 indexes over
-        //Has the date currently passed? If so target fifteen days behind the current index
-        if (today.getDate() > date.getDate()) {
-          predictedBal[index - 16] = predictedBal[index - 16] - element[3];
-          assocDetails[index - 16].push(element[1]);
+    });
+    //Check budget for expenses
+    expenses.forEach((element) => {
+      if (element[2] === date.getDate() && element[0] !== "w") {
+        //Monthly Income
+        if (element[0] === "m") {
+          //Made money this day, so we add to this day's balance
+          predictedBal[index] = predictedBal[index] - element[3];
+          // Log element's description value to the description Array (compensate for muliple entries on a day)
+          assocDetails[index].push(element[1]);
+        } else if (element[0] === "b") {
+          //Bi-Weekly or Salary Income
+          predictedBal[index] = predictedBal[index] - element[3];
+          assocDetails[index].push(element[1]);
+          //Add same balance to the predictedBal 15 indexes over
+          //Has the date currently passed? If so target fifteen days behind the current index
+          if (today.getDate() > date.getDate()) {
+            predictedBal[index - 16] = predictedBal[index - 16] - element[3];
+            assocDetails[index - 16].push(element[1]);
+          } else {
+            predictedBal[index + 15] = predictedBal[index + 15] - element[3];
+            assocDetails[index + 15].push(element[1]);
+          }
         } else {
-          predictedBal[index + 15] = predictedBal[index + 15] - element[3];
-          assocDetails[index + 15].push(element[1]);
+        }
+      } else if (element[0] === "w") {
+        //Get the targeted day of the week
+        let targetDay = element[2];
+        //Check if this day's weekday matches the targetDay
+        if (date.getDay() === targetDay) {
+          predictedBal[index] = predictedBal[index] - element[3];
+          assocDetails[index].push(element[1]);
         }
       } else {
+        //Element did not match index
       }
-    } else if (element[0] === "w") {
-      //Get the targeted day of the week
-      let targetDay = element[2];
-      //Check if this day's weekday matches the targetDay
-      if (date.getDay() === targetDay) {
-        predictedBal[index] = predictedBal[index] - element[3];
-        assocDetails[index].push(element[1]);
-      }
-    } else {
-      //Element did not match index
-    }
-  });
+    });
+  }
+  let data = Array.from({ length: 31 }, () => []);
+  for (var index = 0; index <= 30; index++) {
+    data[index].push(assocDates[index]);
+    data[index].push(predictedBal[index]);
+    data[index].push(assocDetails[index]);
+  }
+  console.log(data);
+  var dataString = JSON.stringify(data);
+  
+  }
+  
+
 }
-let data = Array.from({ length: 31 }, () => []);
-for (var index = 0; index <= 30; index++) {
-  data[index].push(assocDates[index]);
-  data[index].push(predictedBal[index]);
-  data[index].push(assocDetails[index]);
-}
-console.log(data);
-var dataString = JSON.stringify(data);
+export default Budget;
